fa6fad9615b9e2170a27153b07948cfa2991c610
gaox <gx_zone@126.com>
2019-12-18 20:53:31 +0800

    [dev] 网关gateway&主要功能父模块

---
+++ ad-gateway/src/main/java/net/gaox/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package net.gaox.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author gaox·Eric
+ * @site gaox.net
+ * @date 2019/12/18 19:51
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ ad-gateway/src/main/java/net/gaox/gateway/filter/AccessLogFilter.java
@@ -0,0 +1,51 @@
+package net.gaox.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>  </p>
+ *
+ * @author gaox·Eric
+ * @site gaox.net
+ * @date 2019/12/18 19:53
+ */
+@Slf4j
+@Component
+public class AccessLogFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SEND_RESPONSE_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        Long startTime = (Long) context.get("startTime");
+        String uri = request.getRequestURI();
+        long duration = System.currentTimeMillis() - startTime;
+
+        log.info("uri: " + uri + ", duration: " + duration / 100 + "ms");
+
+        return null;
+    }
+}
